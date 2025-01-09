3bd47c375e751bda7c2876eb3f3bcfef53cdf7fa
kostiantyn_ivanchenko <kokos2702@gmail.com>
2025-01-09 01:01:13 +0100

    new

---
+++ src/components/Product.jsx
@@ -0,0 +1,12 @@
+// src/Product.jsx
+
+export default function Product() {
+     return (
+     <div>
+     <h2>Tacos</h2>
+             <p>Price: 999 credits</p>
+         </div>
+     );
+};
+
+    
